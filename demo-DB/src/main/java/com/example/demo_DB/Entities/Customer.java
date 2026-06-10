e6ae527b012708fc4145578bd17ef9eead25e8ae
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-10 16:47:26 +0400

    Implement validation rules

--- demo-DB/src/main/java/com/example/demo_DB/Entities/Customer.java
+++ demo-DB/src/main/java/com/example/demo_DB/Entities/Customer.java
@@ -1,6 +1,9 @@
 package com.example.demo_DB.Entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,9 +20,17 @@ public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer customerID;
+
+    @NotBlank(message = "Customer Name Cannot be Empty")
     private String customerName;
+
+    @Column(unique = true)
     private Integer accountNumber;
+
+    @PositiveOrZero(message = "Balance Cannot be Negative")
     private Double balance;
+
+    @Email(message = "Invalid Email")
     private String email;
     private String phoneNumber;
 }
